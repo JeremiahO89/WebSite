325c00710f10b8c84051ce34a48c9c352d1027a0
JeremiahO89 <technologyfrog89@gmail.com>
2024-03-05 14:23:23 -0500

    resume page competed

--- Version2/popUpScript.js
+++ Version2/popUpScript.js
@@ -1,11 +1,38 @@
 const showResumePopup = document.querySelector(".resume-button");
-const popUpContainer = document.querySelector(".popUp__container");
-const closeBtn = document.querySelector(".close-btn");
+const popUpContainerR = document.querySelector(".popUp__containerR");
+const closeBtnR = document.querySelector(".close-btnR");
 
 showResumePopup.onclick = () => {
-    popUpContainer.classList.add('active');
+    popUpContainerR.classList.add('active');
 }
 
-closeBtn.onclick = () => {
-    popUpContainer.classList.remove('active');
+closeBtnR.onclick = () => {
+    popUpContainerR.classList.remove('active');
+}
+
+
+const showTranscriptPopup = document.querySelector(".transcript-button");
+const popUpContainerT = document.querySelector(".popUp__containerT");
+const closeBtnT = document.querySelector(".close-btnT");
+
+showTranscriptPopup.onclick = () => {
+    popUpContainerT.classList.add('active');
+}
+
+closeBtnT.onclick = () => {
+    popUpContainerT.classList.remove('active');
+}
+
+
+
+const showCertificationsPopup = document.querySelector(".certification-button");
+const popUpContainerC = document.querySelector(".popUp__containerC");
+const closeBtnC = document.querySelector(".close-btnC");
+
+showCertificationsPopup.onclick = () => {
+    popUpContainerC.classList.add('active');
+}
+
+closeBtnC.onclick = () => {
+    popUpContainerC.classList.remove('active');
 }
