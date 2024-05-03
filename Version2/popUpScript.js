2bd3e018e8323308c4566ac7ddd15fee773178f8
JeremiahO89 <technologyfrog89@gmail.com>
2024-05-03 16:35:32 -0400

    work

--- Version2/popUpScript.js
+++ Version2/popUpScript.js
@@ -5,11 +5,9 @@ const closeBtnR = document.querySelector(".close-btnR");
 showResumePopup.onclick = () => {
     popUpContainerR.classList.add('active');
 }
-
 closeBtnR.onclick = () => {
     popUpContainerR.classList.remove('active');
 }
-
 
 const showTranscriptPopup = document.querySelector(".transcript-button");
 const popUpContainerT = document.querySelector(".popUp__containerT");
@@ -18,12 +16,9 @@ const closeBtnT = document.querySelector(".close-btnT");
 showTranscriptPopup.onclick = () => {
     popUpContainerT.classList.add('active');
 }
-
 closeBtnT.onclick = () => {
     popUpContainerT.classList.remove('active');
 }
-
-
 
 const showCertificationsPopup = document.querySelector(".certification-button");
 const popUpContainerC = document.querySelector(".popUp__containerC");
@@ -32,7 +27,6 @@ const closeBtnC = document.querySelector(".close-btnC");
 showCertificationsPopup.onclick = () => {
     popUpContainerC.classList.add('active');
 }
-
 closeBtnC.onclick = () => {
     popUpContainerC.classList.remove('active');
 }
